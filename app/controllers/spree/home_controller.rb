f07c57b80daaa0012f0db21ebb1e305473ec658c
Shane Kelly <shanekweb@icloud.com>
2015-07-09 15:34:57 +0100

    testing controller home

--- app/controllers/spree/home_controller.rb
+++ app/controllers/spree/home_controller.rb
@@ -9,8 +9,12 @@ module Spree
       @products = @searcher.retrieve_products.ascend_by_master_price
       @taxonomies = Spree::Taxonomy.includes(root: :children)
       @user = Spree::User.first
-      company = Company.where(user_id: 1).present?
-      @c = @user.company
+      company = Company.where(user_id: 1)
+      if company !nil || company.user_id !nil
+        @c = @user.company
+      else
+        redirect_to root_path
+      end
     end
 
   end
